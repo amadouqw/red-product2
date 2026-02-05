68632ef3e81f8ba541ba6221157ddaad563c2b10
DESKTOP-5UL4J7C\BMD Tech <amadou527@gmail.com>
2026-02-05 20:08:16 +0100

    Fix: add vite dependency

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route, Navigate } from "react-router-dom"
+import Login from "./Login"
+import Dashboard from "./Dashboard"
+import Hotels from "./Hotels"
+
+function App() {
+  const isAuth = localStorage.getItem("auth")
+
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+
+      <Route
+        path="/dashboard"
+        element={isAuth ? <Dashboard /> : <Navigate to="/" />}
+      />
+
+      <Route
+        path="/hotels"
+        element={isAuth ? <Hotels /> : <Navigate to="/" />}
+      />
+    </Routes>
+  )
+}
+
+export default App
